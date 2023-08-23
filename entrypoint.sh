a3da72cf84a2c08d395fe8a7888d751c2d75d538
Satcom <47340064+Satcomx00-x00@users.noreply.github.com>
2023-08-23 16:33:42 +0200

    update entrpyoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,9 @@
 #! /bin/bash
 source /usr/lib/openfoam/etc/bashrc
 echo "Starting...."
-
+cd /workdir
 ulimit -s unlimited ulimit -v unlimited
+blockMesh
 # mpiexec -n $MPI simpleFoam $FOAM_DIR_PATH
-mpirun -np $MPI simpleFoam $FOAM_DIR_PATH
+mpirun -np $MPI simpleFoam
 # mpirun -np 2 simpleFoam
