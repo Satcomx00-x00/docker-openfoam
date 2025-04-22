5e0eba38edb56976f1d9b5c97c7fbce8cae22e60
Satcom <47340064+Satcomx00-x00@users.noreply.github.com>
2025-04-22 15:58:16 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,21 +5,17 @@
 # ARGUMENTS = -parallel
 cd /workdir
 chmod +xrw -R /workdir
-
-
 # Define color codes
 GREEN='\033[0;32m'
 YELLOW='\033[0;33m'
 RED='\033[0;31m'
 NC='\033[0m' # No Color
-
 # Function to print colored messages
 print_message() {
     local message="$1"
     local color="$2"
     echo -e "${color}${message}${NC}"
 }
-
 # Function to display a shell table
 display_table() {
     local header="$1"
@@ -28,64 +24,51 @@ display_table() {
     printf "%s\n" "$header"
     printf "%s\n" "${rows[@]}"
 }
-
 cd /workdir
 mkdir -p OpenFoam
 cd OpenFoam
 # script terminal.log
 print_message "Unzipping $ZIP_ARCHIVE_INPUT ..." $GREEN
 unzip -o "/workdir/$ZIP_ARCHIVE_INPUT"
-
 # Source OpenFOAM bashrc
 print_message "Sourcing OpenFOAM bashrc..." $GREEN
 source /usr/lib/openfoam/etc/bashrc
-
 # Check if OpenFOAM environment is sourced successfully
 if [ -z "$WM_PROJECT_DIR" ]; then
     print_message "Failed to source OpenFOAM bashrc. Exiting." $RED
     exit 1
 fi
-
 # Set working directory
-WORKDIR="/workdir/$ZIP_ARCHIVE_INPUT"
+# Extract the filename without .zip extension
+ZIP_BASE=$(basename "$ZIP_ARCHIVE_INPUT" .zip)
+WORKDIR="/workdir/$ZIP_BASE"
 cd "$WORKDIR" || exit
-
 # Set ulimit
 ulimit -s unlimited
 ulimit -v unlimited
-
 # Run blockMesh
 print_message "Running blockMesh..." $GREEN
 blockMesh
-
 FOAM_DIR_PATH="$WM_PROJECT_DIR"
 print_message "Running $MODE with $MPI MPI processes..." $GREEN
-
 # Display a summary table
 header="Simulation Summary"
 rows=("Input Archive: $ZIP_ARCHIVE_INPUT" "Output Archive: $ZIP_OUTPUT_FOLDER" "MPI Processes: $MPI" "Mode: $MODE" "Arguments: $ARGUMENTS")
 display_table "$header" "${rows[@]}"
-
-
 # Redirect mpirun output to a log file
 mpirun -n $MPI $MODE $ARGUMENTS  # 2>&1
-
 # Check for errors
 if [ $? -eq 0 ]; then
     print_message "$MODE completed successfully." $GREEN
 else
     print_message "$MODE encountered an error. Please check the log." $RED
 fi
-
 ZIP_OUTPUT_FOLDER="$ZIP_ARCHIVE_INPUT-output.zip"
-
 # Zip output folder
 print_message "Zipping $ZIP_ARCHIVE_INPUT to $ZIP_OUTPUT_FOLDER ..." $GREEN
-zip -r "/workdir/$ZIP_OUTPUT_FOLDER" "/workdir/$ZIP_ARCHIVE_INPUT" 2>&1 || {
+zip -r "/workdir/$ZIP_OUTPUT_FOLDER" "/workdir/$ZIP_BASE" 2>&1 || {
     print_message "Failed to zip $ZIP_ARCHIVE_INPUT. Exiting." $RED
     exit 1
 }
-
 print_message "Successfully zipped $ZIP_OUTPUT_FOLDER." $GREEN
-
 print_message "Work done, you can find your outputs to $ZIP_OUTPUT_FOLDER in your personal volume."
